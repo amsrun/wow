3a9ff12857c3ede16a9badd215bb6147671ae8a7
Le Yang <le.yang@gmail.com>
2023-01-20 20:15:44 +0000

    1/19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CopyPastaComponent } from './copy-pasta/copy-pasta.component';
 import { DiscVsHolyComponent } from './disc-vs-holy/disc-vs-holy.component';
 import { PowerWordShieldComponent } from './power-word-shield/power-word-shield.component';
+import { UIComponent } from './ui/ui.component';
 
 const routes: Routes = [
   { path: 'copy-pasta', component: CopyPastaComponent },
   { path: 'disc-vs-holy', component: DiscVsHolyComponent },
   { path: 'pws', component: PowerWordShieldComponent },
+  { path: 'ui', component: UIComponent },
   { path: '', redirectTo: 'copy-pasta', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { CopyPastaComponent } from './copy-pasta/copy-pasta.component';
 import { DiscVsHolyComponent } from './disc-vs-holy/disc-vs-holy.component';
 import { PowerWordShieldComponent } from './power-word-shield/power-word-shield.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { UIComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CopyPastaComponent,
     DiscVsHolyComponent,
-    PowerWordShieldComponent
+    PowerWordShieldComponent,
+    UIComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
